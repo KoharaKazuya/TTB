df34fe7c913aff014711893e3bfd64f41523828f
Kohara <reflect.results@gmail.com>
2012-11-07 19:41:59 +0900

    表の大きさをゲーム設定に。

--- ttb/Unit.java
+++ ttb/Unit.java
@@ -15,10 +15,6 @@ public abstract class Unit implements InputListener {
 	/** 自フィールドの表オブジェクト */
 	protected Table table;
 	public Table getTable() { return table; }
-	//表の縦の長さ
-	public final static int rows=18;
-	//表の横の長さ
-	public final static int columns=22;
 
 	public String[] alphabet = {"a","b","c","d","e","f","g","h","i","j","k","l","m","n",
 								"o","p","q","r","s","t","u","v","w","x","y","z"};
@@ -52,39 +48,32 @@ public abstract class Unit implements InputListener {
 	 * 表の生成
 	 */
 	public void generateTable() {
-		// テスト用 : TODO
-		if ( true ) {
-			// tableのxyとwordのxyが逆になってると思う。
-			table = new Table(rows, columns);
-			/*画像読み込み*/
-			WordList wo = WordList.getInstance();
+		// ゲーム設定の読み込み
+		GameSetting setting = GameSetting.getInstance();
+		int rows = setting.getTableRows();
+		int columns = setting.getTableColumns();
 
-			int len = wo.stringArray.length;
-			int x, y, r;
-			String enwd, jawd;
-			for(int i=0;i<10000;i++){
-		        Random rnd = new Random();
-		        x = rnd.nextInt(columns);
-		        y = rnd.nextInt(rows);
-		        r = rnd.nextInt(len);
-		        enwd = wo.stringArray[r][0];
-		        jawd = wo.stringArray[r][1];
-				table.addObtainableWord( new Word(enwd, jawd, x, y, Word.Orient.DOWN) );
-		        x = rnd.nextInt(columns);
-		        y = rnd.nextInt(rows);
-		        r = rnd.nextInt(len);
-		        enwd = wo.stringArray[r][0];
-		        jawd = wo.stringArray[r][1];
-				table.addObtainableWord( new Word(enwd, jawd, x, y, Word.Orient.RIGHT) );
-			}
-			
-			//テスト用
-			/*
-			table.addObtainableWord( new Word("test", 0, 0, Word.Orient.DOWN) );
-			table.addObtainableWord( new Word("eat", 18, 5, Word.Orient.RIGHT) );
-			table.addObtainableWord( new Word("play", 18, 14, Word.Orient.DOWN) );
-			table.addObtainableWord( new Word("eat", 7, 4, Word.Orient.RIGHT) );
-			*/
+		table = new Table(rows, columns);
+		/*画像読み込み*/
+		WordList wo = WordList.getInstance();
+
+		int len = wo.stringArray.length;
+		int x, y, r;
+		String enwd, jawd;
+		Random rnd = new Random();
+		for(int i=0;i<10000;i++){
+			x = rnd.nextInt(columns);
+			y = rnd.nextInt(rows);
+			r = rnd.nextInt(len);
+			enwd = wo.stringArray[r][0];
+			jawd = wo.stringArray[r][1];
+			table.addObtainableWord( new Word(enwd, jawd, x, y, Word.Orient.DOWN) );
+			x = rnd.nextInt(columns);
+			y = rnd.nextInt(rows);
+			r = rnd.nextInt(len);
+			enwd = wo.stringArray[r][0];
+			jawd = wo.stringArray[r][1];
+			table.addObtainableWord( new Word(enwd, jawd, x, y, Word.Orient.RIGHT) );
 		}
 	}
 
