d22e6507ee8470bf5cd04a61b11cfe2138dd3f13
KoharaKazuya <reflect.results@gmail.com>
2012-11-20 21:35:39 +0900

    タイトル画面ステートの追加

---
+++ ttb/GameStateTitle.java
@@ -0,0 +1,48 @@
+package ttb;
+
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.BasicGameState;
+import org.newdawn.slick.state.StateBasedGame;
+
+/**
+ * タイトル画面
+ * 
+ * @author Kohara
+ */
+public class GameStateTitle extends BasicGameState {
+	/** ステートID */
+	private int stateID;
+	
+	public GameStateTitle(int stateID) {
+		this.stateID = stateID;
+	}
+
+	@Override
+	public void init(GameContainer container, StateBasedGame game)
+			throws SlickException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void render(GameContainer container, StateBasedGame game, Graphics g)
+			throws SlickException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void update(GameContainer container, StateBasedGame game, int delta)
+			throws SlickException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public int getID() {
+		return stateID;
+	}
+
+}
